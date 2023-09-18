68648552f96c49863949e7f394f6a23e6c2ea5d4
truongltse05868 <truongltse05868@fpt.edu.vn>
2023-09-18 20:43:51 +0700

    angibanoi

--- src/App.js
+++ src/App.js
@@ -1,22 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import foodData from "./food-database.js"; // Import dữ liệu từ tệp JavaScript
+import Stack from "@mui/material/Stack";
+// import Button from '@mui/material/Button';
+import { Button } from "@mui/material";
 
 function App() {
+  const [randomFood, setRandomFood] = useState(false);
+
+  const getRandomFood = () => {
+    if (foodData.length > 0) {
+      const randomIndex = Math.floor(Math.random() * foodData.length);
+      return foodData[randomIndex];
+    }
+    return null;
+  };
+
+  const handleGenerateFood = () => {
+    const randomFood = getRandomFood();
+    setRandomFood(randomFood);
+  };
+  const handleResetFood = () => {
+    setRandomFood(false);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hôm nay ăn gì nào bạn, bạn chọn đi:</p>
+        <Stack spacing={2}>
+          {" "}
+          {/* Use Stack component */}
+          <Button
+            variant="contained" // Tùy chỉnh kiểu nút
+            color="primary" // Tùy chỉnh màu sắc
+            size="large" // Tùy chỉnh kích thước
+            onClick={handleGenerateFood}
+          >
+            Generate Food
+          </Button>
+          <Button variant="outlined" onClick={handleResetFood}>Reset</Button>
+        </Stack>
+        {randomFood && (
+          <div>
+            <p>
+              Đi ăn: <strong>{randomFood.name}</strong> thôi!!!!!
+            </p>
+            <img
+              src={randomFood.image}
+              alt={randomFood.name}
+              style={{
+                maxWidth: "100%",
+                maxHeight: "400px",
+                width: "auto",
+                height: "auto",
+              }}
+            />
+          </div>
+        )}
       </header>
     </div>
   );
---
+++ src/food-database.js
@@ -0,0 +1,24 @@
+const foodData = [
+  {
+    name: "Ốc Tô",
+    image: "images/octo.jpg",
+  },
+  {
+    name: "Yakimono",
+    image: "images/yakimono.jpg",
+  },
+  {
+    name: "Pizza 4'P",
+    image: "images/pizza4p.jpg",
+  },
+  {
+    name: "Bún bò Huế",
+    image: "images/bunbo.jpg",
+  },
+  {
+    name: "Lẩu Manwah",
+    image: "images/manwa.jpg",
+  },
+];
+
+export default foodData;
