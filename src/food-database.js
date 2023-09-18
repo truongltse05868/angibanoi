90be74d8c53f9b7ceb909b08441d13112bf1fa23
truonglt0410 <nhimshinobi@gmail.com>
2023-09-18 21:53:47 +0700

    edit link anh

--- src/food-database.js
+++ src/food-database.js
@@ -1,27 +1,27 @@
 const foodData = [
   {
     name: "Ốc Tô",
-    image: "images/octo.jpg",
+    image: "https://nhaphonet.vn/wp-content/uploads/2023/05/oc-to-sai-gon-lang-ha-4.jpg",
   },
   {
     name: "Yakimono",
-    image: "images/yakimono.jpg",
+    image: "https://yakimono.com.vn/temp/-uploaded-khuyenmai_yaki2_thumb_420x275.png",
   },
   {
     name: "Pizza 4'P",
-    image: "images/pizza4p.jpg",
+    image: "https://pizza4ps.com/wp-content/uploads/2023/07/PizzaBYO-1.png",
   },
   {
     name: "Bún bò Huế",
-    image: "images/bunbo.jpg",
+    image: "https://cdn.tgdd.vn/Files/2018/04/01/1078873/nau-bun-bo-hue-cuc-de-tai-nha-tu-vien-gia-vi-co-san-202109161718049940.jpg",
   },
   {
     name: "Lẩu Manwah",
-    image: "images/manwa.jpg",
+    image: "https://pasgo.vn/Upload/anh-chi-tiet/nha-hang-manwah-lotte-lieu-giai-2-normal-2216800354196.webp",
   },
   {
     name: "Lẩu Riêu",
-    image: "images/manwa.jpg",
+    image: "https://i-giadinh.vnecdn.net/2022/10/14/-2636-1665740316.jpg",
   },
 ];
 
